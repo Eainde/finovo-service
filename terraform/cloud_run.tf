65281c6c2fc18104908e504b100b6a88579194bf
Akshay Dipta <akshay.dipta@gmail.com>
2025-07-22 15:50:07 +0100

    Updating credentials

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -60,4 +60,15 @@ resource "google_cloud_run_v2_service" "main" {
   depends_on = [
     google_project_service.apis,
   ]
+
+  lifecycle {
+    precondition {
+      condition     = data.google_artifact_registry_repository.main.repository_id != null
+      error_message = "The specified Artifact Registry repository '${var.existing_repo_name}' was not found in region '${var.gcp_region}'. Please ensure it exists and the name is correct before running apply."
+    }
+    precondition {
+      condition     = data.google_sql_database_instance.main.private_ip_address != null && data.google_sql_database_instance.main.private_ip_address != ""
+      error_message = "The specified Cloud SQL instance '${var.db_instance_name}' does not have a Private IP address enabled or it could not be read. Please ensure it is enabled in the GCP console and re-run the workflow."
+    }
+  }
 }
